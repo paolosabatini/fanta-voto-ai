654c3b31d1c743719ccf64fe894e8f0c2b119549
Paolo <paolo.sabatini@cern.ch>
2022-09-09 18:48:38 +0200

    fix on goal scored

--- preprocess/augmenter.py
+++ preprocess/augmenter.py
@@ -3,7 +3,7 @@
         
     
 def goal_scored ( df ):
-    df['goal_taken'] = df.apply (lambda x : x['goal_away'] if x['Squadra'] == x['away'] else x['goal_home'], axis=1)
+    df['goal_scored'] = df.apply (lambda x : x['goal_away'] if x['Squadra'] == x['away'] else x['goal_home'], axis=1)
 
     
 def goal_taken ( df ):
