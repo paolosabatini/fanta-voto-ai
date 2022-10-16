7e16737417bea998d2fae5cc091692c907a8c637
Paolo <paolo.sabatini@cern.ch>
2022-10-16 22:13:20 +0200

    setting up environment to operate with py3

---
+++ deactivate_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "[pyenv] Setting back the system version"
+pyenv local system
---
+++ setup_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+myenv="dev-py3.10"
+echo "[pyenv] Setting up environment: "${myenv}
+pyenv local dev-py3.10
